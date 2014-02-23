f3991b7d23d0766f39160199cd08ef04b0971ad3
Julien Tallec <service.github@tjtallec.ca>
2014-02-22 20:05:24 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WebQRor::Application.config.secret_key_base = '07e0b1032ee18c442b03f62dde858469e165021b1edcfa059dd44d2cfa81083b7b02415b3675fa05bf447ba1ecfebe0a4e3e907dae59f5793db52714ef688876'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WebQRor::Application.config.session_store :cookie_store, key: '_WebQRor_session'
